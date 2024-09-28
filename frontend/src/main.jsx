d746b92c3942acf63aad6472ff98e1e03beab54a
bibaswankundu123 <bibaswankundu4@gmail.com>
2024-09-28 12:06:10 +0530

    first-commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import StoreContextProvider from './context/StoreContext.jsx';
+
+// Import createRoot from react-dom/client
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+  </BrowserRouter>
+);
