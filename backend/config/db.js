fc7728bf7c4ccc24678b30f76db47197a7c49e57
Bibaswan Kundu <160421670+bibaswankundu123@users.noreply.github.com>
2024-09-28 14:33:06 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://bibaswan:bibaswan1234@cluster0.aaiut.mongodb.net/foody').then(()=>console.log("DB Connected"));
+    await mongoose.connect('').then(()=>console.log("DB Connected"));
 }
